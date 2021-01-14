c616a474e707e7dfed2884368ec80047effdc213
Alice Ferrazzi <alicef@gentoo.org>
2021-01-15 07:04:16 +0900

    Copy kernel image to fileserver

---
+++ to_fileserver.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+ARCH=$1
+BUILDER_NAME=$2
+BUILD_NUMBER=$3
+FILESERVER=/var/www/fileserver/
+
+
+IMAGE_PATH=linux-amd64-build/arch/x86_64/boot/bzImage
+COPY_IMAGE_PATH="${FILESERVER}"/"${BUILDER_NAME}"/"${BUILD_NUMBER}"/
+mkdir -p "${COPY_IMAGE_PATH}"
+chmod -R 755 "${COPY_IMAGE_PATH}"
+
+cp -rf "${IMAGE_PATH}" "${COPY_IMAGE_PATH}"
+chmod 755 "${COPY_IMAGE_PATH}"/*
