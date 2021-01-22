825a232665f0126321325a8248f40597dd9a4d3d
Alice Ferrazzi <alicef@gentoo.org>
2021-01-22 20:14:33 +0900

    Fix patch hash using the patch folder instead of README
    Signed-off-by: Alice Ferrazzi <alicef@gentoo.org>

--- kcidb/get_patches.py
+++ kcidb/get_patches.py
@@ -1,24 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import glob
+import ntpath
+
 def get_patches_list(kernel_major_version):
     patches_list=[]
-    file_name="../linux-patches/0000_README"
-    with open(file_name, 'r') as read_obj:
-        patch_name=""
-        patch_url=""
-        count=0
-        for line in read_obj:
-            if "Patch:" in line:
-                patch_name=line.strip("\n")
-                patch_name=patch_name.replace("Patch:","")
-                patch_name=patch_name.strip(" ")
-                count += 1
-            if "From:" in line:
-                patch_url="https://raw.githubusercontent.com/GKernelCI/linux-patches/" + kernel_major_version + "/" + patch_name
-                count += 1
-            if count==2:
-                patches_list.append({'name':patch_name, 'url':patch_url})
-                count=0
+    path = '../linux-patches/*.patch'
+    files = glob.glob(path)
+    for filename in files:
+        patchbasename=(ntpath.basename(filename))
+        print(patchbasename)
+        patch_name=patchbasename
+        patch_url="https://raw.githubusercontent.com/GKernelCI/linux-patches/" + kernel_major_version + "/" + patchbasename
+        patches_list.append({'name':patch_name, 'url':patch_url})
     return(patches_list)
-
