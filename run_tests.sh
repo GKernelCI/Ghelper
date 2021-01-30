c1c919b53ce0ed6d88f18be8f4b0e009c3a53079
Alice Ferrazzi <alicef@gentoo.org>
2021-01-31 05:14:48 +0900

    Get latest stage3 build
    Signed-off-by: Alice Ferrazzi <alicef@gentoo.org>

--- run_tests.sh
+++ run_tests.sh
@@ -10,8 +10,6 @@ SCRIPT_DIR=$(cd "$(dirname "$0")"|| exit;pwd)
 
 tmpyml=$(mktemp "/tmp/XXXXXX.yml")
 
-KERNEL_STORAGE_URL=http://"${STORAGE_SERVER}"/"${BUILDER_NAME}"/"${BUILD_NUMBER}"/bzImage
-sed -e "s@KERNEL_IMAGE_URL@${KERNEL_STORAGE_URL}@g" ${SCRIPT_DIR}/lava/job/gentoo-boot.yml > $tmpyml
 
 start_job_get_job_id() {
   lavacli -i buildbot jobs submit "$tmpyml"
@@ -60,6 +58,22 @@ display_lava_url () {
   echo "LAVAJOB_URL=http://$LAVA_SERVER/scheduler/job/$job_id"
 }
 
+configure_lava_boot() {
+  KERNEL_STORAGE_URL=http://"${STORAGE_SERVER}"/"${BUILDER_NAME}"/"${BUILD_NUMBER}"/bzImage
+  tmptxt=$(mktemp "/tmp/XXXXXX.txt")
+  tmpdigest=$(mktemp "/tmp/XXXXXX.digest")
+  wget http://gentoo.mirrors.ovh.net/gentoo-distfiles/releases/amd64/autobuilds/latest-stage3-amd64.txt -qO "$tmptxt"
+  file_url=$(awk 'NR==3{ print $1 }' < "$tmptxt")
+  wget http://gentoo.mirrors.ovh.net/gentoo-distfiles/releases/amd64/autobuilds/"$file_url".DIGESTS -qO "$tmpdigest"
+  file_hash=$(awk 'NR==2{ print $1 }' < "$tmpdigest")
+  rootfs_fullurl=http://gentoo.mirrors.ovh.net/gentoo-distfiles/releases/amd64/autobuilds/"$file_url"
+  sed -e "s@KERNEL_IMAGE_URL@${KERNEL_STORAGE_URL}@g" -e "s@ROOTFS_HASH@${file_hash}@g" \
+  -e "s@ROOTFS_URL@${rootfs_fullurl}@g" "${SCRIPT_DIR}"/lava/job/gentoo-boot.yml > "$tmpyml"
+  rm -rf "$tmptxt"
+  rm -rf "$tmpdigest"
+}
+
+configure_lava_boot
 job_id=$(start_job_get_job_id)
 check_job_state
 check_tasks
