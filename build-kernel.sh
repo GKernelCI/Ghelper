c7bab395648051d16cfd3ada3837b64871f328dd
Corentin Labbe <clabbe.montjoie@gmail.com>
2021-02-10 10:19:28 +0100

    Give arguments a name

--- build-kernel.sh
+++ build-kernel.sh
@@ -5,11 +5,14 @@ set -e
 MAKEOPTS="-j$(( $(getconf _NPROCESSORS_ONLN) + 1 ))"
 
 if [ $# -lt 1 ]; then
-	echo "Usage: $(basename $0) arch"
+	echo "Usage: $(basename $0) arch BUILD_NAME BUILD_NUMBER SOURCEDIR"
 	exit 1
 fi
 
 ARCH=$1
+BUILD_NAME=$2
+BUILD_NUMBER=$3
+SOURCEDIR=$4
 
 FDIR="$(dirname $(realpath $0))/linux-$ARCH-build/"
 
@@ -17,7 +20,7 @@ echo "DEBUG: output is in $FDIR"
 
 MAKEOPTS="$MAKEOPTS O=$FDIR"
 
-case $2 in
+case $BUILD_NAME in
 *)
 make $MAKEOPTS
 ;;
