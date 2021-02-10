a7ffe770af5b11f4d307f903598cde5182d94756
Corentin Labbe <clabbe.montjoie@gmail.com>
2021-02-10 11:23:45 +0100

    Add action parameters, for handling later config,build,modules

--- build-kernel.sh
+++ build-kernel.sh
@@ -5,7 +5,7 @@ set -e
 MAKEOPTS="-j$(( $(getconf _NPROCESSORS_ONLN) + 1 ))"
 
 if [ $# -lt 1 ]; then
-	echo "Usage: $(basename $0) arch BUILD_NAME BUILD_NUMBER SOURCEDIR"
+	echo "Usage: $(basename $0) arch BUILD_NAME BUILD_NUMBER SOURCEDIR [build|modules]"
 	exit 1
 fi
 
@@ -13,19 +13,32 @@ ARCH=$1
 BUILD_NAME=$2
 BUILD_NUMBER=$3
 SOURCEDIR=$4
+ACTION=$5
+
+# hacks before Gbuildbot has all args
+if [ "$2" = 'modules' ];then
+	ACTION='modules'
+fi
+if [ -z "$ACTION" ];then
+	ACTION=build
+fi
 
 build() {
 	FDIR="$(dirname $(realpath $0))/linux-$ARCH-build/"
 
-	echo "DEBUG: output is in $FDIR"
+	echo "DEBUG: $ACTION for $ARCH to $FDIR"
 	MAKEOPTS="$MAKEOPTS O=$FDIR"
 
-	case $BUILD_NAME in
-	*)
+	case $ACTION in
+	build)
 		make $MAKEOPTS
 	;;
 	modules)
 		make $MAKEOPTS modules
+	;;
+	*)
+		echo "ERROR: unknow action: $ACTION"
+		exit 1
 	;;
 	esac
 }
