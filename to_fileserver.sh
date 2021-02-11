3b5a069e94ce767561e3fe28f6b7d13b0b8f9125
Corentin Labbe <clabbe.montjoie@gmail.com>
2021-02-11 16:49:47 +0100

    Move code in copy_artifact function
    Simply move code in a function and call it.

--- to_fileserver.sh
+++ to_fileserver.sh
@@ -5,12 +5,16 @@ BUILDER_NAME=$(echo $2 | sed 's,:,_,g')
 BUILD_NUMBER=$3
 FILESERVER=/var/www/fileserver/
 
-FDIR="linux-$ARCH-build/"
+copy_artifact() {
+	FDIR="linux-$ARCH-build/"
 
-IMAGE_PATH="$FDIR/arch/x86/boot/bzImage"
-COPY_IMAGE_PATH="${FILESERVER}"/"${BUILDER_NAME}"/"${BUILD_NUMBER}"/
-mkdir -p "${COPY_IMAGE_PATH}"
-chmod -R 755 "${COPY_IMAGE_PATH}"
+	IMAGE_PATH="$FDIR/arch/x86/boot/bzImage"
+	COPY_IMAGE_PATH="${FILESERVER}"/"${BUILDER_NAME}"/"${BUILD_NUMBER}"/
+	mkdir -p "${COPY_IMAGE_PATH}"
+	chmod -R 755 "${COPY_IMAGE_PATH}"
 
-cp -rf "${IMAGE_PATH}" "${COPY_IMAGE_PATH}"
-chmod 755 "${COPY_IMAGE_PATH}"/*
+	cp -rf "${IMAGE_PATH}" "${COPY_IMAGE_PATH}"
+	chmod 755 "${COPY_IMAGE_PATH}"/*
+}
+
+copy_artifact
