a3d3aee48274359605db4c3f201b91bbe4d57618
Corentin Labbe <clabbe.montjoie@gmail.com>
2021-02-15 11:00:55 +0100

    Implement a new way to generate LAVA jobs
    The current way to generate jobs via a file+sed is not suitable for
    generating jobs for lot of different qemu.
    
    Using a template is necessary, bringing the need of python and jinja2.
    Furthermore, python will be necessary for easily testing job results.

---
+++ gentoo_get_stage_url.sh
@@ -0,0 +1,188 @@
+#!/bin/sh
+
+DEBUG=0
+RFS_BASE=http://gentoo.mirrors.ovh.net/gentoo-distfiles/
+RFS_BASE=http://ftp.free.fr/mirrors/ftp.gentoo.org/
+ARCH_OPTION=""
+
+debug() {
+	if [ $DEBUG -eq 1 ];then
+		echo "$*"
+	fi
+}
+
+while [ $# -ge 1 ]
+do
+	case $1 in
+	-d)
+		DEBUG=1
+		shift
+	;;
+	--arch)
+		shift
+		if [ -z "$1" ];then
+			echo "ERROR: missing subargument"
+			exit 1
+		fi
+		debug "DEBUG: set arch to $1"
+		ARCH=$1
+		case $1 in
+		armel)
+			ARCH=arm
+		;;
+		armbe)
+			echo "ERROR: armbe not supported by gentoo"
+			exit 1
+		;;
+		powerpc)
+			ARCH=ppc
+		;;
+		esac
+		if [ "$ARCH" = 'x86_64' ];then
+			ARCH=amd64
+		fi
+		SARCH=$ARCH
+		shift
+	;;
+	*)
+		echo "ERROR: unknow argument $1"
+		exit 1
+	;;
+	esac
+done
+
+debug "end of args"
+
+if [ -z "$ARCH" ];then
+	echo "ERROR: arch is not set"
+	exit 1
+fi
+
+CHECK_SIG=1
+
+found_latest()
+{
+	RFS_BPATH=/releases/$ARCH/autobuilds
+	BASEURL=$RFS_BASE$RFS_BPATH
+	case $ARCH in
+	arm)
+		SARCH=armv7a_hardfp
+	;;
+	arm64)
+		RFS_BPATH=/experimental/$ARCH
+		BASEURL=$RFS_BASE$RFS_BPATH
+		curl -s "$BASEURL/" > index.html
+		if [ ! -e index.html ];then
+			echo "ERROR: no index from $BASEURL"
+			return 1
+		fi
+		STAGE3=$(grep -o '"stage3-arm64-[0-9]*.tar.bz2"' index.html | cut -d'"' -f2)
+		LATEST=$STAGE3
+		CHECK_SIG=0
+		rm index.html
+		return 0
+	;;
+	sparc64)
+		RFS_BPATH=/releases/sparc/autobuilds
+		BASEURL=$RFS_BASE$RFS_BPATH
+	;;
+	ppc64)
+		RFS_BPATH=/releases/ppc/autobuilds
+		BASEURL=$RFS_BASE$RFS_BPATH
+	;;
+	hppa)
+		ARCH_OPTION="1.1"
+	;;
+	m68k)
+		RFS_BPATH=/experimental/$ARCH/
+		BASEURL=$RFS_BASE$RFS_BPATH
+		LATEST="stage3-m68k-20130509.tar.bz2"
+		CHECK_SIG=0
+		return
+	;;
+	mips)
+		RFS_BPATH=/experimental/$ARCH/autobuilds
+		BASEURL=$RFS_BASE$RFS_BPATH
+		# TODO
+		exit 1
+	;;
+	x86)
+		SARCH=i686
+	;;
+	esac
+
+	LATEST_TXT="latest-stage3-${SARCH}${ARCH_OPTION}.txt"
+	curl -s "$BASEURL/$LATEST_TXT" > $LATEST_TXT
+	RET=$?
+	if [ $RET -ne 0 ];then
+		echo "ERROR: fail to grab $BASEURL/$LATEST_TXT"
+		exit 1
+	fi
+	echo "ROOTFS_LATEST=$BASEURL/$LATEST_TXT"
+	LATEST=$(grep -v ^# $LATEST_TXT | cut -d' ' -f1)
+	return 0
+}
+
+found_latest || exit $?
+
+debug "Found latest success"
+
+DIGESTS_ASC=$(basename "$LATEST.DIGESTS.asc")
+DIGESTS=$(basename "$LATEST.DIGESTS")
+
+curl -s "$BASEURL/$LATEST.DIGESTS" > $DIGESTS
+if [ $? -ne 0 ];then
+	echo "ERROR: fail to download $BASEURL/$LATEST.DIGESTS"
+	rm $LATEST_TXT
+	exit 1
+fi
+
+if [ $CHECK_SIG -eq 1 ];then
+	curl -s "$BASEURL/$LATEST.DIGESTS.asc" > $DIGESTS_ASC
+	RET=$?
+	if [ $RET -ne 0 ];then
+		echo "ERROR: fail to download $BASEURL/$LATEST.DIGESTS.asc"
+		rm latest-stage3-$SARCH.txt
+		rm latest-stage3-$SARCH.DIGESTS
+		exit 1
+	fi
+
+	gpg --batch -q --verify "$DIGESTS_ASC" >gpg.out 2>gpg.err
+	RET=$?
+	if [ $RET -ne 0 ];then
+		echo "ERROR: GPG fail to verify"
+		cat gpg.out
+		cat gpg.err
+		rm gpg.err gpg.out
+		exit 1
+	fi
+	rm gpg.err gpg.out
+fi
+
+echo "ROOTFS_URL=$BASEURL/$LATEST"
+echo "ROOTFS_BASE=$RFS_BASE"
+echo "ROOTFS_PATH=$RFS_BPATH/$LATEST"
+#cat $(basename $LATEST.DIGESTS) |
+while read -r line
+do
+	echo "$line" | grep -q SHA512
+	RET=$?
+	if [ $RET -eq 0 ];then
+		read line
+		LATEST_BASENAME=$(basename "$LATEST")
+		echo "$line" | grep -q "$LATEST_BASENAME$"
+		RET=$?
+		if [ $RET -eq 0 ];then
+			ROOTFS_SHA512=$(echo "$line" | cut -d' ' -f1)
+			echo "ROOTFS_SHA512=$ROOTFS_SHA512"
+		fi
+	fi
+done < "$DIGESTS"
+
+rm "$DIGESTS"
+if [ $CHECK_SIG -eq 1 ];then
+	rm "$DIGESTS_ASC"
+fi
+if [ -e "$LATEST_TXT" ];then
+	rm "$LATEST_TXT"
+fi
--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# check the artifact directory of a build for generating LAVA jobs
+# LAVA lifecycle (Generating, submit, check) is done by run_tests.py
+
 ARCH=$1
 # make cannot handle ":" in a path, so we need to replace it
 BUILDER_NAME=$(echo $2 | sed 's,:,_,g')
@@ -9,70 +12,30 @@ LAVA_SERVER=140.211.166.173:10080
 STORAGE_SERVER=140.211.166.171:8080
 SCRIPT_DIR=$(cd "$(dirname "$0")"|| exit;pwd)
 
-tmpyml=$(mktemp "/tmp/XXXXXX.yml")
-
-
-start_job_get_job_id() {
-  lavacli -i buildbot jobs submit "$tmpyml"
-  rm -f "$tmpyml"
+usage() {
+	echo "Usage: $0 ARCH BUILDER_NAME BUILD_NUMBER"
 }
 
-get_job_state() {
-  lavacli --id buildbot jobs show "${job_id}" | grep "state       :" | awk '{print $3}'
-}
+if [ -z "$ARCH" ] ;then
+	usage
+	exit 1
+fi
 
-get_failed_results() {
-  lavacli -i buildbot results "${job_id}" | grep fail | awk '{ print $2 }'
-}
+if [ -z "$BUILDER_NAME" ] ;then
+	usage
+	exit 1
+fi
 
-check_job_state() {
-  job_state="$(get_job_state)"
-  if [[ $job_state != "Finished" ]]; then
-    echo -n -e "The job $job_id is $job_state"
-    while [[ $job_state != "Finished" ]]; do
-      job_state="$(get_job_state)"
-      sleep 5
-      echo -n -e "."
-    done
-  fi
-  echo The job "${job_id}" is "${job_state}"
-}
+if [ -z "$BUILD_NUMBER" ] ;then
+	usage
+	exit 1
+fi
 
-skip_usual_failing_task() {
-  failed_tasks=$(get_failed_results)
-  line=1
-  for output_line in $failed_tasks
-  do
-    line_string=$(awk -v linevar=$line 'NR==linevar {print; exit}' skip/kselftest_skiplist )
-    if [ "$output_line" = "$line_string" ]; then
-      echo "$line_string [Skipped]"
-    else
-      echo "$output_line [Failed] not on the skip list"
-      exit 1
-    fi
-    line=$(($line + 1))
-  done
-}
-
-display_lava_url () {
-  echo "LAVAJOB_URL=http://$LAVA_SERVER/scheduler/job/$job_id"
-}
-
-configure_lava_boot() {
-  KERNEL_STORAGE_URL=http://"${STORAGE_SERVER}/${BUILDER_NAME}/$ARCH/${BUILD_NUMBER}/$defconfig/$toolchain/bzImage"
-  latest_stage3_amd64=$(curl -s http://gentoo.mirrors.ovh.net/gentoo-distfiles/releases/amd64/autobuilds/latest-stage3-amd64.txt)
-  rootfs_url=$(echo "$latest_stage3_amd64" | awk 'NR==3{ print $1 }')
-  rootfs_digests_file=$(curl -s http://gentoo.mirrors.ovh.net/gentoo-distfiles/releases/amd64/autobuilds/"$rootfs_url".DIGESTS)
-  rootfs_digest=$(echo "$rootfs_digests_file" | awk 'NR==2{ print $1 }')
-  rootfs_fullurl=http://gentoo.mirrors.ovh.net/gentoo-distfiles/releases/amd64/autobuilds/"$rootfs_url"
-  sed -e "s@KERNEL_IMAGE_URL@${KERNEL_STORAGE_URL}@g" -e "s@ROOTFS_HASH@${rootfs_digest}@g" \
-  -e "s@ROOTFS_URL@${rootfs_fullurl}@g" "${SCRIPT_DIR}"/lava/job/gentoo-boot.yml > "$tmpyml"
-  add_kselftest
-}
-
-add_kselftest(){
-  cat "${SCRIPT_DIR}"/lava/job/gentoo-kselftest.yml >> "$tmpyml"
-}
+# permit to override default
+if [ -e config.ini ];then
+	echo "INFO: Loading default from config.ini"
+	. config.ini
+fi
 
 SCANDIR="$FILESERVER/$BUILDER_NAME/$ARCH/$BUILD_NUMBER/"
 if [ ! -e "$SCANDIR" ];then
@@ -87,15 +50,20 @@ do
 	for toolchain in gcc
 	do
 		echo "CHECK: toolchain $toolchain"
-		configure_lava_boot
-		job_id=$(start_job_get_job_id)
-		display_lava_url
-		check_job_state
-		skip_usual_failing_task
 		echo "BOOT: $SCANDIR/$defconfig/$toolchain"
+		./run_tests.py --arch $ARCH \
+			--buildname $BUILDER_NAME \
+			--buildnumber $BUILD_NUMBER \
+			--toolchain $toolchain \
+			--defconfig $defconfig \
+			--fileserver $FILESERVER \
+			--fileserverfqdn http://$STORAGE_SERVER/ \
+			--waitforjobsend
 		if [ $? -ne 0 ];then
 			echo "ERROR: there is some fail"
 			exit 1
 		fi
 	done
 done
+
+exit 0
