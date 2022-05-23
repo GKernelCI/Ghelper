dbca33da939fc45b17e5a963f73b3c15a1ce111a
Corentin Labbe <clabbe.montjoie@gmail.com>
2022-05-23 14:22:38 +0200

    Handle error code in docker_emerge.sh
    If a command fail, exit status remains ok.
    We need to fail is a command fail.
    
    Signed-off-by: Corentin Labbe <clabbe.montjoie@gmail.com>

--- docker_emerge.sh
+++ docker_emerge.sh
@@ -8,19 +8,20 @@ for kernel_sources in "$@"; do
   if [[ "${kernel_sources}" =~ .ebuild$ ]]; then
     if [[ "${kernel_sources}" =~ sources ]]; then
       gentoo_rootfs=$(docker run -d --name gentoo"${currentdate}" gentoo/stage3-amd64:latest tail -f /dev/null)
-      docker exec "${gentoo_rootfs}" wget --quiet https://github.com/gentoo/gentoo/archive/master.zip -O /master.zip
-      docker exec "${gentoo_rootfs}" unzip -q master.zip
+      echo "DEBUG: use $gentoo_rootfs as docker image"
+      docker exec "${gentoo_rootfs}" wget --quiet https://github.com/gentoo/gentoo/archive/master.zip -O /master.zip || exit $?
+      docker exec "${gentoo_rootfs}" unzip -q master.zip || exit $?
       libself_recent_ebuild=$(docker exec "${gentoo_rootfs}" find /gentoo-master/dev-libs/libelf/ -iname "*.ebuild" | sort -Vr | head -n 1)
       bc_recent_ebuild=$(docker exec "${gentoo_rootfs}" find /gentoo-master/sys-devel/bc -iname "*.ebuild" | sort -Vr | head -n 1)
-      docker exec "${gentoo_rootfs}" /usr/bin/ebuild "${libself_recent_ebuild}" clean merge
-      docker exec "${gentoo_rootfs}" /usr/bin/ebuild "${bc_recent_ebuild}" clean merge
-      docker exec "${gentoo_rootfs}" /usr/bin/ebuild /gentoo-master/"${kernel_sources}" clean merge
-      docker exec "${gentoo_rootfs}" ls /usr/src/linux -la
-      docker exec -w /usr/src/linux "${gentoo_rootfs}" make defconfig
-      docker exec -w /usr/src/linux "${gentoo_rootfs}" make $MAKEOPTS "$*"
-      docker cp "${gentoo_rootfs}":/usr/src/linux/arch/x86/boot/bzImage "${FILESERVER}"/"${kernel_sources}"/"${currentdate}"/
-      docker stop "${gentoo_rootfs}"
-      docker rm "${gentoo_rootfs}"
+      docker exec "${gentoo_rootfs}" /usr/bin/ebuild "${libself_recent_ebuild}" clean merge || exit $?
+      docker exec "${gentoo_rootfs}" /usr/bin/ebuild "${bc_recent_ebuild}" clean merge || exit $?
+      docker exec "${gentoo_rootfs}" /usr/bin/ebuild /gentoo-master/"${kernel_sources}" clean merge || exit $?
+      docker exec "${gentoo_rootfs}" ls /usr/src/linux -la || exit $?
+      docker exec -w /usr/src/linux "${gentoo_rootfs}" make defconfig || exit $?
+      docker exec -w /usr/src/linux "${gentoo_rootfs}" make $MAKEOPTS "$*" || exit $?
+      docker cp "${gentoo_rootfs}":/usr/src/linux/arch/x86/boot/bzImage "${FILESERVER}"/"${kernel_sources}"/"${currentdate}"/ || exit $?
+      docker stop "${gentoo_rootfs}" || exit $?
+      docker rm "${gentoo_rootfs}" || exit $?
     fi
   fi
 done
