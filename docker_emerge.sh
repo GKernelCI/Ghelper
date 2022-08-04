78a159f7d9f4f9f4b2b75d54a548707001cb1d64
Alice Ferrazzi <alicef@gentoo.org>
2022-08-04 10:28:18 +0900

    docker_emerge: be sure to remove the Gentoo docker container on EXIT
    Signed-off-by: Alice Ferrazzi <alicef@gentoo.org>

--- docker_emerge.sh
+++ docker_emerge.sh
@@ -4,10 +4,21 @@ MAKEOPTS="-j$(( $(getconf _NPROCESSORS_ONLN) + 1 ))"
 currentdate=$(date +%Y%m%d_%H%M%S)
 FILESERVER=/var/www/fileserver/
 
+gentoo_rootfs=$(docker run -d --name gentoo"${currentdate}" gentoo/stage3:latest tail -f /dev/null)
+
+# cleaning gentoo docker container
+function cleanup {
+  echo "removing docker image ${gentoo_rootfs}"
+  docker stop "${gentoo_rootfs}" || exit $?
+  docker rm "${gentoo_rootfs}" || exit $?
+}
+
+# be sure to remove gentoo docker container on EXIT
+trap cleanup EXIT
+
 for kernel_sources in "$@"; do 
   if [[ "${kernel_sources}" =~ .ebuild$ ]]; then
     if [[ "${kernel_sources}" =~ sources ]]; then
-      gentoo_rootfs=$(docker run -d --name gentoo"${currentdate}" gentoo/stage3:latest tail -f /dev/null)
       echo "DEBUG: use $gentoo_rootfs as docker image"
       # gentoolkit need PYTHON_TARGETS
       docker exec "${gentoo_rootfs}" sed -i '$ a PYTHON_TARGETS="python3_9"' /etc/portage/make.conf || exit $?
@@ -24,9 +35,6 @@ for kernel_sources in "$@"; do
       docker exec -w /usr/src/linux "${gentoo_rootfs}" make defconfig || exit $?
       docker exec -w /usr/src/linux "${gentoo_rootfs}" make $MAKEOPTS || exit $?
       docker cp "${gentoo_rootfs}":/usr/src/linux/arch/x86/boot/bzImage "${FILESERVER}"/"${kernel_sources}"/"${currentdate}"/ || exit $?
-      docker stop "${gentoo_rootfs}" || exit $?
-      docker rm "${gentoo_rootfs}" || exit $?
     fi
   fi
 done
-
